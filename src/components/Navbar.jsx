dd6c1030a5ed43f55754ea4f647958d43a2de991
NidaWaheed <109359002+Nidachaudary@users.noreply.github.com>
2024-09-12 04:19:17 +0500

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
+// import navbar.module.css for navbar styling
 import styles from "./Navbar.module.css";
+// import fontawesome for using the icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar({ setDarkMode, darkMode }) {
+  // State to track if the navbar is collapsed
   const [isCollapsed, setIsCollapsed] = useState(true);
 
+  // Function to toggle the collapse state
   const handleToggle = () => {
     setIsCollapsed(!isCollapsed);
   };
 
   return (
     <nav className={`${styles.nav} navbar navbar-expand-lg navbar-light`}>
-      <div className="container-fluid position-relative">
+      <div className="container-fluid">
+        {/* Centering the Website Name */}
+        <div
+          className={`d-flex flex-grow-1  ${styles.justify} ms-5 position-relative`}
+        >
+          <Link className="navbar-brand text-white fs-2 fw-bold" to="/">
+            TALVA
+          </Link>
+        </div>
+
         {/* Mobile Menu Button */}
         <button
-          className="navbar-toggler fs-6 order-1"
+          className="navbar-toggler fs-6 me-2"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -32,21 +45,12 @@ function Navbar({ setDarkMode, darkMode }) {
           )}
         </button>
 
-        {/* Middle: Website Name */}
-        <div className="d-flex justify-content-center align-items-center position-absolute start-50 top-50 translate-middle">
-          <Link className="navbar-brand text-white fs-2 fw-bold" to="/">
-            TALVA
-          </Link>
-        </div>
-
         {/* Right side: Navigation Links */}
         <div
-          className={`collapse navbar-collapse ${
-            isCollapsed ? "" : "show"
-          } order-2`}
+          className={`collapse navbar-collapse ${isCollapsed ? "" : "show"}`}
           id="navbarNav"
         >
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto  mb-2 mb-lg-0">
             <li className="nav-item">
               <NavLink
                 className={`${styles.color} nav-link fs-5`}
@@ -75,10 +79,7 @@ function Navbar({ setDarkMode, darkMode }) {
                 onClick={() => setDarkMode((prevState) => !prevState)}
                 className={`${styles.button} px-2`}
               >
-                <FontAwesomeIcon
-                  icon={darkMode ? faMoon : faSun}
-                  className=""
-                />
+                <FontAwesomeIcon icon={darkMode ? faMoon : faSun} />
               </button>
             </li>
           </ul>
